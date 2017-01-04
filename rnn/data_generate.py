ab79dc3dcc31ff063b863af4eda511bb40406ec9
diggerdu <diggerdu@hotmail.com>
2017-01-05 00:05:53 +0800

    rnn

---
+++ rnn/data_generate.py
@@ -0,0 +1,16 @@
+import librosa
+import os
+import numpy as np
+import scipy.io.wavfile as wave
+
+N_FFT = 1024
+AUDIO_PATH = '../Suites-for-solo-cello/mo1_16k.wav'
+OUTPUT_PATH  = './data/train.npy'
+fs, X = wave.read(AUDIO_PATH) 
+print np.max(X)
+S = np.abs(librosa.stft(X, N_FFT))
+print np.max(S)
+
+
+np.save(OUTPUT_PATH, S)
+
--- rnn/rnn_without_log.py
+++ rnn/rnn_without_log.py
@@ -3,37 +3,33 @@ import numpy as np
 
 
 ## hyper parameter
+n_step = 12  # bidirectional
 n_input = 257
 n_output = 256
 n_hidden = 256
-n_h_layer = 16
-learning_rate = 0.00001
-batch_size = 512
+n_h_layer = 4
+learning_rate = 0.00002
+batch_size = 768
 train_iters = np.inf
 save_step = 30000
 ## load data
 input_data = np.log1p(np.load("./data/train.npy").T[:5000, 0:n_input]) / 12.0
 target_data= np.log1p(np.load("./data/train.npy").T[:5000, n_input:]) / 8.0
 
-with tf.device('/gpu:0'):
-    x = tf.placeholder("float", [None, n_input])
+with tf.device('/gpu:1'):
+    x = tf.placeholder("float", [None, n_step, n_input])
+    rnn_input = tf.transpose(x, [1, 0, 2])
+    rnn_input = tf.unstack(rnn_input, axis=0)
     y = tf.placeholder("float", [None, n_output])
-
-    op_list = list()
-    op_list.append(x)
     
-    w_input = tf.Variable(tf.truncated_normal([n_input, n_hidden], stddev=0.1))
-    b_input = tf.Variable(tf.constant(0., shape=[n_hidden]))
-    op_list.append(tf.nn.relu(tf.matmul(op_list[-1], w_input) + b_input))
-    for i in range(n_h_layer):
-        w = tf.Variable(tf.truncated_normal([n_hidden, n_hidden], stddev=0.1))
-        b = tf.Variable(tf.constant(0., shape=[n_hidden]))
-        op_list.append(tf.nn.relu(tf.matmul(op_list[-1], w) + b))
-    
+    rnn_cell = tf.nn.rnn_cell.BasicRNNCell(n_hidden)
+    fw_cell = tf.nn.rnn_cell.MultiRNNCell([rnn_cell] * n_h_layer, state_is_tuple=True)
+    #bw_cell = tf.nn.rnn_cell.MultiRNNCell([rnn_cell] * n_h_layer, state_is_tuple=True)
+    outputs,_ = tf.nn.rnn(fw_cell, rnn_input, dtype=tf.float32)
     w_output = tf.Variable(tf.truncated_normal([n_hidden, n_output], stddev=0.1))
     b_output = tf.Variable(tf.constant(0., shape=[n_output]))
-    op_list.append(tf.sigmoid(tf.matmul(op_list[-1], w_output) + b_output))
-    loss = tf.reduce_mean(tf.multiply(tf.log1p(y), tf.abs(tf.subtract(y, op_list[-1]))))
+    logits = tf.sigmoid(tf.matmul(outputs[-1], w_output) + b_output)
+    loss = tf.reduce_mean(tf.multiply(y, tf.abs(tf.subtract(y, logits))))
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 init = tf.initialize_all_variables()
@@ -42,8 +38,9 @@ sess = tf.Session()
 sess.run(init)
 step = 1
 while step * batch_size < train_iters: 
-    idx = np.random.choice(input_data.shape[0], batch_size)
-    batch_inputs = input_data[idx]
+    idx = np.random.choice(input_data.shape[0] - n_step*2, batch_size) + n_step
+    batch_inputs = np.asarray([input_data[i+1-n_step:i+1] for i in idx.tolist()])
+    print batch_inputs.shape
     batch_targets = target_data[idx]
     loss_, _ = sess.run([loss, optimizer], feed_dict={x:batch_inputs, y:batch_targets})
 
