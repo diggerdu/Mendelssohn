4d9e5f4cc06a9e0a8ab4a2dd6a5dfdda9003d28b
diggerdu <diggerdu@hotmail.com>
2017-02-10 11:11:39 +0000

    refine ops

--- cnn/ops.py
+++ cnn/ops.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.layers as ly
 
 
 def ten_sh(tensor):
@@ -7,7 +8,7 @@ def ten_sh(tensor):
 
 def conv2d(input_, o_dim, k_size, st, name='conv2d'):
     with tf.variable_scope(name):
-        init = tf.contrib.layers.xavier_initializer_conv2d()
+        init = ly.xavier_initializer_conv2d()
         fil = tf.get_variable('co_f', k_size+\
                 [ten_sh(input_)[-1], o_dim],initializer=init)
         co = tf.nn.conv2d(input_, fil, strides=[1]+st+[1], \
@@ -17,6 +18,13 @@ def conv2d(input_, o_dim, k_size, st, name='conv2d'):
         return co
 
 def deconv2d(input_, o_size, k_size, name='deconv2d'):
+    assert np.mod(o_size[1::3], ten_sh(input_)[1::3]) == [0,0]
+    with tf.variable_scope(name):
+        init = ly.xavier_initializer_conv2d()
+        output = ly.convolution2d_transpose(input_, num_outputs=o_size[-1], \
+                kernel_size=k_size, stride=np.divide(o_size[1::3], ten_sh(input_)[1::3]), padding='SAME', weights_initializer=init)
+        return output
+'''
     with tf.variable_scope(name):
         init = tf.contrib.layers.xavier_initializer_conv2d()
         fil = tf.get_variable('dc_f', k_size+[o_size[-1], \
@@ -26,5 +34,5 @@ def deconv2d(input_, o_size, k_size, name='deconv2d'):
         bia = tf.get_variable('dc_b', [output_shape[-1]])
         dc = tf.nn.bias_add(dc, bia)
         return dc
-
+'''
 
